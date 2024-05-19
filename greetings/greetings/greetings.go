c9ffa1a1d389e886ea6732988b1ef2b76591f67c
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2024-05-19 20:36:27 +0100

    Learning about maps

--- greetings/greetings/greetings.go
+++ greetings/greetings/greetings.go
@@ -3,7 +3,20 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
 )
+
+func Hellos(names []string) (map[string]string, error) {
+    messages := make(map[string]string)
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        messages[name] = message
+    }
+    return messages, nil
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -14,6 +27,21 @@ func Hello(name string) (string, error) {
 
     // If a name was recieved, return a value that embeds the name
     // in a greeting message
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+    // a slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a randomly selected format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
 }
--- greetings/hello/hello.go
+++ greetings/hello/hello.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"os"
-
 	"example.com/greetings"
 	"github.com/charmbracelet/log"
-	"github.com/erikgeiser/promptkit/textinput"
 )
 
 func main() {
     // Get a greeting message and print it.
-    input := textinput.New("What is your name?")
-    input.InitialValue = os.Getenv("USER")
-    input.Placeholder = "Your name cannot be empty"
+    names := []string{"Gladys", "Samantha", "Darrin"}
 
-    name, err := input.RunPrompt()
+    messages, err := greetings.Hellos(names)
 
     if err != nil {
-        log.Fatal("Error: failed to retrieve user input", "err", err)
+        log.Fatal("Error: empty name", "err", err)
     }
 
-    message, err2 := greetings.Hello(name)
-    if err2 != nil {
-        log.Fatal("Error: empty name", "err2", err2)
-    }
-
-    log.Print(message)
+    log.Print(messages)
 }
