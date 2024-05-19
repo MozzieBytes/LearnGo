a7b320b6a34086cb3f760f86bf4cf03e585c390a
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2024-05-19 20:36:27 +0100

    Adding post request

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -26,9 +26,25 @@ var albums = []album {
     { ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99 },
 }
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BidJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
 }
