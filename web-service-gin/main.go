62ab5e553f98840b82d86c9e5919dc8427e1dfe4
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2024-05-19 20:36:27 +0100

    Finishing off web dev first lesson

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -14,16 +14,25 @@ type album struct {
     Price  float64 `json:"price"`
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
-
 // albums slice to seed record album data.
 var albums = []album {
     { ID: "1", Title: "Blue Train", Artist: "JohnColtrane", Price: 56.99 },
     { ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99 },
     { ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99 },
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
+
+    router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
 }
 
 // postAlbums adds an album from JSON received in the request body.
@@ -41,11 +50,19 @@ func postAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.POST("/albums", postAlbums)
+// getAlbumById locates the album whose ID value matches the getAlbumById
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
 
-    router.Run("localhost:8080")
+    // Loop over the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
